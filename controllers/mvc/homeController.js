be99296f019157cf522fb60d20b829ffdc99aa13
Gustavo Militao <gustavo.h.militao@gmail.com>
2019-06-03 14:01:14 -0300

    changes

--- controllers/mvc/homeController.js
+++ controllers/mvc/homeController.js
@@ -1,9 +1,4 @@
 exports.home = function (req, res) {
-    var cookie = req.cookies['user'];
-    if (cookie) {
-        var path = require("path");
-        res.render(path.join(__dirname+"/../../views/home/index.html"));
-    } else {
-        res.redirect("/");
-    }
+    var path = require("path");
+    res.render(path.join(__dirname + "/../../views/home/index.html"));
 };
